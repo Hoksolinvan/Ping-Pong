e46e2082cfee8b440c164e4f354893c9dcb1296c
Hoksolinvan Chhun <158796823+Hoksolinvan@users.noreply.github.com>
2026-04-14 11:02:21 -0700

    Update Ping-Pong.py

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -10,8 +10,13 @@ pygame.display.set_caption("Ping-Pong game")
 main_loop = True
 clock = pygame.time.Clock()
 
-first_user = pygame.Rect(0,HEIGHT//2,20,100)
-second_user = pygame.Rect(WIDTH-20,HEIGHT//2,20,100)
+first_position_x = 0
+first_position_y = HEIGHT//2
+
+second_position_x = WIDTH-20
+second_position_y = HEIGHT//2
+
+
 
 user_speed = 10
 
@@ -24,20 +29,31 @@ while main_loop:
       main_loop = False
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_w:
-        pass
+
+        if first_position_y-user_speed >=0:
+          first_position_y-=user_spped
 
       if event.key == pygame.K_s:
-        pass
 
+        if first_position_y+user_speed+100<=HEIGHT:
+          first_position_y+=user_speed
 
       if event.key == pygame.K_UP:
-        pass
 
+        if second_position_y-user_speed >=0:
+          second_position_y-=user_speed
+        
       if event.key == pygame.K_DOWN:
-        pass
+        if second_position_y+user_speed+100<=HEIGHT:
+          second_position_y+=user_speed
+
+
+
+  first_user = pygame.Rect(first_position_x,first_position_y,20,100)
+  second_user = pygame.Rect(second_position_x,second_position_y,20,100)
 
   
-
+  
   pygame.display.flip()
 
   clock.tick(FPS)
